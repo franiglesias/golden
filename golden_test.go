3cf8d614b54bf46c4329a0822945509836c60638
Fran Iglesias <franiglesias@mac.com>
2024-01-17 15:22:23 +0100

    Improves a lot the design of TestVerify, restarting the FS abstraction so each test starts fresh

--- golden_test.go
+++ golden_test.go
@@ -6,23 +6,39 @@ import (
 )
 
 func TestVerify(t *testing.T) {
+	var fs *golden.MemFs
+	var gt golden.TSpy
 
-	// Replace testing.T with a double to be able to spy results
-	gt := golden.TSpy{
-		T: t,
+	setUp := func(t *testing.T) {
+		// Passing t in each setup guarantees that we are using the right name for the snapshot
+
+		// Avoid using real filesystem in test
+		// Inits the fs, so it's empty on each test
+
+		fs = golden.NewMemFs()
+
+		// Inits global G for using test filesystem
+
+		golden.G = golden.NewUsingFs(fs)
+
+		// Replace testing.T with this double to allow spying results
+
+		gt = golden.TSpy{
+			T: t,
+		}
 	}
 
 	t.Run("should create snapshot if not exists", func(t *testing.T) {
-		fs := golden.NewMemFs()
-		golden.G = golden.NewUsingFs(fs)
+		setUp(t)
+
 		subject := "some subject."
 		golden.Verify(t, subject)
 		golden.AssertSnapshotWasCreated(t, fs, "__snapshots/TestVerify/should_create_snapshot_if_not_exists.snap")
 	})
 
 	t.Run("should write subject as snapshot content", func(t *testing.T) {
-		fs := golden.NewMemFs()
-		golden.G = golden.NewUsingFs(fs)
+		setUp(t)
+
 		subject := "some output."
 		golden.Verify(t, subject)
 		expected := []byte(subject)
@@ -30,34 +46,27 @@ func TestVerify(t *testing.T) {
 	})
 
 	t.Run("should not alter snapshot when it exists", func(t *testing.T) {
-		fs := golden.NewMemFs()
-		golden.G = golden.NewUsingFs(fs)
+		setUp(t)
+
 		subject := "some output."
 		golden.Verify(&gt, subject)
 		modified := "different output."
 		golden.Verify(&gt, modified)
-		expected := []byte(subject)
-		// When used this way, t.Name() is not used
-		golden.AssertContentWasStored(t, fs, "__snapshots/TestVerify.snap", expected)
+
+		want := []byte(subject)
+
+		golden.AssertContentWasStored(t, fs, "__snapshots/TestVerify/should_not_alter_snapshot_when_it_exists.snap", want)
 	})
 
-	t.Run("should detect difference", func(t *testing.T) {
-		fs := golden.NewMemFs()
-		golden.G = golden.NewUsingFs(fs)
-		subject := "original output."
-		golden.Verify(&gt, subject)
-		modified := "different output."
-		golden.Verify(&gt, modified)
+	t.Run("should detect and report differences by line", func(t *testing.T) {
+		setUp(t)
+
+		// Sets the snapshot
+		golden.Verify(&gt, "original output.")
+		// Changes happened. Verify against existing snapshot
+		golden.Verify(&gt, "different output.")
+
 		golden.AssertFailedTest(t, &gt)
-	})
-
-	t.Run("should report differences", func(t *testing.T) {
-		fs := golden.NewMemFs()
-		golden.G = golden.NewUsingFs(fs)
-		subject := "original output."
-		golden.Verify(&gt, subject)
-		modified := "different output."
-		golden.Verify(&gt, modified)
 		golden.AssertReportContains(t, &gt, "-original output.\n+different output.\n")
 	})
 }
